0789f84129bc479cb4d3644d18afc781649c527b
Kimberly Suazo  & Neel Gupta <pair+kimberlysuazo.neelgupta2@devbootcamp.com>
2016-09-16 14:59:58 -0400

    Edit seed file and table migrations

--- web-flashcards-challenge-master/db/migrate/20160916113949_create_games.rb
+++ web-flashcards-challenge-master/db/migrate/20160916113949_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration
   def change
   	create_table :games do |t|
-  		t.integer :deck_id null: false
-  		t.integer :user_id null: false
+  		t.integer :deck_id, null: false
+  		t.integer :user_id, null: false
 
   		t.timestamps
   	end
--- web-flashcards-challenge-master/db/seeds.rb
+++ web-flashcards-challenge-master/db/seeds.rb
@@ -4,15 +4,15 @@ cards = [
 	{ question: "Is a whale a mammal?",
 		answer: "Yes",
 		deck_id: first_deck.id
-	}
+	},
 	{ question: "Is the pope a catholic?",
 		answer: "Yes",
 		deck_id: first_deck.id
-	}
+	},
 	{ question: "Is Manhattan a borough of NYC?",
 		answer: "Yes",
 		deck_id: first_deck.id
-	}
+	},
 	{ question: "Is a wolf a sheep?",
 		answer: "No",
 		deck_id: first_deck.id
