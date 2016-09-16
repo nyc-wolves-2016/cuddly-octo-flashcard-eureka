a0a1ea8f2eed7943081454b8afb54b08feb502f8
Neel Gupta & Thomas Graves <pair+neelgupta2.thomaslogangraves@devbootcamp.com>
2016-09-16 17:33:36 -0400

    Finish models for game.

--- web-flashcards-challenge-master/app/models/game.rb
+++ web-flashcards-challenge-master/app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ActiveRecord::Base
   belongs_to :user
   belongs_to :deck
   has_many :responses
-  has_many :cards, through: :responses
+  has_many :cards, through: :deck
 
   def correct_answers
   	self.responses.count { |response| response.correct? }
@@ -13,5 +13,7 @@ class Game < ActiveRecord::Base
   	self.responses.count
   end
 
-
+  def cards_in_play
+  	self.responses.reject { |response| response.correct? }.map { |response| response.card }
+	end
 end
